a7685079f46b7ca4e4dec410756bdb10d98f44bc
BashaerAlf <alfuhaidbashayeer@gmail.com>
2024-09-26 01:57:20 -0700

    Initial Commit

---
+++ tst/tstApp.swift
@@ -0,0 +1,17 @@
+//
+//  tstApp.swift
+//  tst
+//
+//  Created by BASHAER AZIZ on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct tstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
